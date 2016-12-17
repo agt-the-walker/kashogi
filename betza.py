ba413d08189436b915f04941a014026ffe6e5d2a
Jerome Zago <jerome.zago@gmail.com>
2016-12-18 00:26:42 +0100

    Piece directions from Betza notation

---
+++ betza.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import re
+
+class Betza:
+    def __init__(self, notation):
+        tokens = re.findall(r"([a-z]*)([A-Z])([1-9][0-9]*)?", notation)
+        if not tokens:
+            raise ValueError('No token found')
+
+        self.directions = {}  # coordinate => best range
+        for token in tokens:
+            self._parse(*token)
+
+    def _parse(self, modifiers, piece, range):
+        if not range:
+            if piece in ['B', 'Q', 'R']:
+                range = 0  # i.e. unlimited
+            else:
+                range = 1  # i.e. minimal
+        else:
+            range = int(range)
+
+        if piece == 'A':
+            self._add_movement(2, 2, modifiers, range)
+        elif piece in ['B', 'F']:
+            self._add_movement(1, 1, modifiers, range)
+        elif piece == 'D':
+            self._add_movement(0, 2, modifiers, range)
+        elif piece in ['K', 'Q']:
+            self._add_movement(0, 1, modifiers, range)
+            self._add_movement(1, 1, modifiers, range)
+        elif piece == 'N':
+            self._add_movement(1, 2, modifiers, range)
+        elif piece in ['R', 'W']:
+            self._add_movement(0, 1, modifiers, range)
+        else:
+            raise ValueError('Unknown piece: ' + piece)
+
+    def _add_movement(self, m, n, modifiers, range):
+        assert m <= n
+
+        if modifiers:
+            if m == 0:  # orthogonal
+                list_modifiers = re.findall(r"[bflr]", modifiers)
+            else:       # diagonal/oblique
+                list_modifiers = [x[0] for x in
+                        re.findall(r"(bl|br|fl|fr|([bflr])(\2?))", modifiers)]
+
+        for coordinate in self._coordinates(m, n):
+            dx, dy = coordinate
+            if not modifiers:
+                self._add_directions(dx, dy, range)
+                continue
+
+            for modifier in list_modifiers:
+                if len(modifier) == 2 and modifier[0] == modifier[1]:
+                    modifier = modifier[0]
+                    repeated = True
+                else:
+                    repeated = False
+
+                # diagonal/oblique
+                if modifier == 'bl' and dx < 0 and dy < 0:
+                    self._add_directions(dx, dy, range)
+                elif modifier == 'br' and dx > 0 and dy < 0:
+                    self._add_directions(dx, dy, range)
+                elif modifier == 'fl' and dx < 0 and dy > 0:
+                    self._add_directions(dx, dy, range)
+                elif modifier == 'fr' and dx > 0 and dy > 0:
+                    self._add_directions(dx, dy, range)
+
+                # all
+                if not(repeated) or abs(dx) < abs(dy):
+                    if modifier == 'b' and dy < 0:
+                        self._add_directions(dx, dy, range)
+                    elif modifier == 'f' and dy > 0:
+                        self._add_directions(dx, dy, range)
+                if not(repeated) or abs(dx) > abs(dy):
+                    if modifier == 'l' and dx < 0:
+                        self._add_directions(dx, dy, range)
+                    elif modifier == 'r' and dx > 0:
+                        self._add_directions(dx, dy, range)
+
+    def _add_directions(self, dx, dy, range):
+        if (dx, dy) in self.directions:
+            # only upgrade range for the better
+            old_range = self.directions[(dx, dy)]
+            if old_range != 0 and (range > old_range or range == 0):
+                self.directions[(dx, dy)] = range
+        else:
+            self.directions[(dx, dy)] = range
+
+    @staticmethod
+    def _coordinates(m, n):
+        assert m <= n
+
+        if m == n:  # diagonal
+            for coordinate in [(-m, -n), (-m, n), (m, -n), (m, n)]:
+                yield coordinate
+        else:       # oblique/orthogonal
+            if m != 0:  # oblique
+                for coordinate in [(-m, n), (m, -n), (n, -m), (-n, m)]:
+                    yield coordinate
+            for coordinate in [(-m, -n), (m, n), (-n, -m), (n, m)]:
+                yield coordinate
---
+++ test_betza.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import unittest
+
+from betza import Betza
+
+class BetzaTestCase(unittest.TestCase):
+    def test_invalid_notation(self):
+        with self.assertRaisesRegex(ValueError, 'No token found'):
+            Betza('')
+        with self.assertRaisesRegex(ValueError, 'No token found'):
+            Betza('#!')
+
+    def test_invalid_piece(self):
+        with self.assertRaisesRegex(ValueError, 'Unknown piece: C'):
+            Betza('C')
+
+
+    def test_notation_order_on_dragon(self):
+        expected = {(-1, 1): 1, (0, 1): 0, (1, 1): 1,
+                    (-1, 0): 0,            (1, 0): 0,
+                    (-1,-1): 1, (0,-1): 0, (1,-1): 1}
+        for notation in ['FR', 'RF', 'KR', 'RK']:
+            self.assertEqual(Betza(notation).directions, expected)
+
+    def test_double_digit_range_on_queen(self):
+        self.assertEqual(Betza('Q12').directions,
+                {(-1, 1): 12, (0, 1): 12, (1, 1): 12,
+                 (-1, 0): 12,             (1, 0): 12,
+                 (-1,-1): 12, (0,-1): 12, (1,-1): 12})
+
+
+    def test_blind_dog(self):
+        self.assertEqual(Betza('fFrlbW').directions,
+                {(-1, 1): 1,            (1, 1): 1,
+                 (-1, 0): 1,            (1, 0): 1,
+                             (0,-1): 1           })
+
+    def test_cloud_eagle(self):
+        self.assertEqual(Betza('fbRfB3K').directions,
+                {(-1, 1): 3, (0, 1): 0, (1, 1): 3,
+                 (-1, 0): 1,            (1, 0): 1,
+                 (-1,-1): 1, (0,-1): 0, (1,-1): 1})
+
+    def test_eagle(self):
+        self.assertEqual(Betza('fBbRWbB2').directions,
+                {(-1, 1): 0, (0, 1): 1, (1, 1): 0,
+                 (-1, 0): 1,            (1, 0): 1,
+                 (-1,-1): 2, (0,-1): 0, (1,-1): 2})
+
+    def test_falcon(self):
+        self.assertEqual(Betza('FfrlW').directions,
+                {(-1, 1): 1, (0, 1): 1, (1, 1): 1,
+                 (-1, 0): 1,            (1, 0): 1,
+                 (-1,-1): 1,            (1,-1): 1})
+
+    def test_heavenly_horse(self):
+        self.assertEqual(Betza('ffbbN').directions,
+                {(-1, 2): 1, (1, 2): 1,
+                 (-1,-2): 1, (1,-2): 1})
+
+    def test_left_quail(self):
+        self.assertEqual(Betza('fRbrBblF').directions,
+                {            (0, 1): 0,
+                 (-1,-1): 1,            (1,-1): 0})
+
+    def test_left_inverted_quail(self):
+        self.assertEqual(Betza('fRfrBflF').directions,
+                {(-1, 1): 1,            (1, 1): 0,
+                             (0, 1): 0           })
+
+    def test_right_quail(self):
+        self.assertEqual(Betza('fRblBbrF').directions,
+                {            (0, 1): 0,
+                 (-1,-1): 0,            (1,-1): 1})
+
+    def test_right_inverted_quail(self):
+        self.assertEqual(Betza('fRflBfrF').directions,
+                {(-1, 1): 0,            (1, 1): 1,
+                             (0, 1): 0           })
+
+    def test_shogi_knight(self):
+        self.assertEqual(Betza('ffN').directions,
+                {(-1, 2): 1, (1, 2): 1})
+
+    def test_treacherous_fox(self):
+        self.assertEqual(Betza('fbWFfbDA').directions,
+                {(-2, 2): 1,             (0, 2): 1,            (2, 2): 1,
+                             (-1, 1): 1, (0, 1): 1, (1, 1): 1,
+
+                             (-1,-1): 1, (0,-1): 1, (1,-1): 1,
+                 (-2,-2): 1,             (0,-2): 1,            (2,-2): 1})
+
+    def test_wide_knight(self):
+        self.assertEqual(Betza('llrrN').directions,
+                {(-2, 1): 1, (2, 1): 1,
+                 (-2,-1): 1, (2,-1): 1})
+
+
+if __name__ == '__main__':
+    unittest.main()
