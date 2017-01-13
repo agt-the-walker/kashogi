e79d18c138d19cd05f607d5908f9797a0064c368
Jerome Zago <jerome.zago@gmail.com>
2017-01-13 23:53:41 +0100

    Group related items using QGraphicsItemGroup
    This makes it easier to position labels relative to the board.

--- position_viewer.py
+++ position_viewer.py
@@ -6,7 +6,7 @@ import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont, QFontMetrics, QPen
 from PyQt5.QtWidgets import QApplication, QGraphicsView, QGraphicsScene, \
-                            QGraphicsSimpleTextItem
+                            QGraphicsSimpleTextItem, QGraphicsItemGroup
 
 from pieces import Pieces
 from position import Position
@@ -30,8 +30,8 @@ class PositionScene(QGraphicsScene):
         if position.num_ranks > len(ascii_lowercase):
             raise ValueError('Too many ranks in position for GUI')
 
-        self._draw_board_labels(position)
         self._draw_board(position)
+        self._draw_board_labels(position)
 
     def _draw_board_labels(self, pos):
         font = QFont(LABEL_FONT)
@@ -39,56 +39,72 @@ class PositionScene(QGraphicsScene):
 
         self._compute_lowercase_max_width(font)
 
+        file_labels = QGraphicsItemGroup()
+        rank_labels = QGraphicsItemGroup()
+
         for i in range(pos.num_files):
             file = pos.num_files - i if pos.player_to_move == 0 else i+1
 
             text = QGraphicsSimpleTextItem(str(file))
             text.setFont(font)
-            text.setPos(LINE_OFFSET + (i + 0.5) * SQUARE_SIZE
-                        - text.boundingRect().width() / 2,
-                        - FILE_LABEL_OFFSET - LABEL_SIZE)
+            text.setPos(i * SQUARE_SIZE - text.boundingRect().width() / 2, 0)
 
-            self.addItem(text)
+            file_labels.addToGroup(text)
+
+        file_labels.setPos(LINE_OFFSET + 0.5 * SQUARE_SIZE,
+                           - FILE_LABEL_OFFSET - LABEL_SIZE)
 
         for i in range(pos.num_ranks):
             rank = pos.num_ranks - i if pos.player_to_move == 1 else i+1
 
             text = QGraphicsSimpleTextItem(chr(ord('a') + rank - 1))
             text.setFont(font)
-            text.setPos(SQUARE_SIZE * pos.num_files + 2 * LINE_OFFSET +
-                        RANK_LABEL_OFFSET +
-                        (self._max_width - text.boundingRect().width()) / 2,
-                        LINE_OFFSET + (i + 0.5) * SQUARE_SIZE
-                        - text.boundingRect().height() / 2)
+            text.setPos((self._max_width - text.boundingRect().width()) / 2,
+                        i * SQUARE_SIZE - text.boundingRect().height() / 2)
 
-            self.addItem(text)
+            rank_labels.addToGroup(text)
+
+        rank_labels.setPos(self._board.boundingRect().width()
+                           + RANK_LABEL_OFFSET,
+                           LINE_OFFSET + 0.5 * SQUARE_SIZE)
+
+        self.addItem(file_labels)
+        self.addItem(rank_labels)
 
     def _compute_lowercase_max_width(self, font):
         fm = QFontMetrics(font)
         self._max_width = max([fm.width(letter) for letter in ascii_lowercase])
 
     def _draw_board(self, pos):
+        board = QGraphicsItemGroup()
+
         pen = QPen()
         pen.setWidth(BOARD_STROKE)
 
-        self.addRect(BOARD_STROKE / 2, BOARD_STROKE / 2,
-                     SQUARE_SIZE * pos.num_files + BOARD_STROKE - LINE_STROKE,
-                     SQUARE_SIZE * pos.num_ranks + BOARD_STROKE - LINE_STROKE,
-                     pen)
+        board.addToGroup(self.addRect(
+            BOARD_STROKE / 2, BOARD_STROKE / 2,
+            SQUARE_SIZE * pos.num_files + BOARD_STROKE - LINE_STROKE,
+            SQUARE_SIZE * pos.num_ranks + BOARD_STROKE - LINE_STROKE,
+            pen))
 
         pen.setWidth(LINE_STROKE)
 
         for file in range(1, pos.num_files):
-            self.addLine(LINE_OFFSET + SQUARE_SIZE * file, LINE_OFFSET,
-                         LINE_OFFSET + SQUARE_SIZE * file,
-                         LINE_OFFSET + SQUARE_SIZE * pos.num_ranks,
-                         pen)
+            board.addToGroup(self.addLine(
+                LINE_OFFSET + SQUARE_SIZE * file, LINE_OFFSET,
+                LINE_OFFSET + SQUARE_SIZE * file,
+                LINE_OFFSET + SQUARE_SIZE * pos.num_ranks,
+                pen))
 
         for rank in range(1, pos.num_ranks):
-            self.addLine(LINE_OFFSET, LINE_OFFSET + SQUARE_SIZE * rank,
-                         LINE_OFFSET + SQUARE_SIZE * pos.num_files,
-                         LINE_OFFSET + SQUARE_SIZE * rank,
-                         pen)
+            board.addToGroup(self.addLine(
+                LINE_OFFSET, LINE_OFFSET + SQUARE_SIZE * rank,
+                LINE_OFFSET + SQUARE_SIZE * pos.num_files,
+                LINE_OFFSET + SQUARE_SIZE * rank,
+                pen))
+
+        self.addItem(board)
+        self._board = board
 
 
 class PositionView(QGraphicsView):
